60c6fc27671c022350e0b8da1288f6f312d3da9c
Yefrid David <yefriddavid@hotmail.com>
2015-09-12 17:23:55 -0500

    Mejoras

---
+++ src/app/Preguntas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+/**
+ *
+ * @author IE_DEVELOPER1
+ */
+public class Preguntas {
+    String name;
+    int Type;
+    int Respuesta;
+    Respuesta[] respuestas = new Respuesta[4];
+    
+}
---
+++ src/util/dataBase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author IE_DEVELOPER1
+ */
+public class dataBase {
+    public void setPregunta(){
+        
+    }
+    public void setTipoPregunta(){
+        
+    }
+}
