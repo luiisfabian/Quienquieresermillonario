fd8582edd459b8a7e7ae7bb7001d7b2e112a8b96
Yefrid David <yefriddavid@hotmail.com>
2015-09-14 07:23:03 -0500

    Recupere el archivo

--- src/util/dataBase.java
+++ src/util/dataBase.java
@@ -8,23 +8,23 @@ public class dataBase {
     public void setPregunta(app.Question p) {
         JSONObject j = new JSONObject();
         try {
-            j.put("nombre", "531328f8616c69133a350000");
-            j.put("pregunta", "1");
-            j.put("call_count", "0");
-            j.put("rating", "3");
+            j.put("description", "");
+            j.put("question", "");
+            j.put("call_count", "");
+            j.put("rating", "");
         } catch (JSONException e1) {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
-        JSONObject json = new JSONObject();
-        try {
-            json.put("email", "a@b.com");
-            json.put("user_post_records", json);
-
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+//        JSONObject json = new JSONObject();
+//        try {
+//            json.put("email", "a@b.com");
+//            json.put("user_post_records", json);
+//
+//        } catch (JSONException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
     }
 
     public void setTipoPregunta() {
