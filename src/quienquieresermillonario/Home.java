61f06d077f87546c5de6d7a26268e38544b2123e
Santiago Vaslo <vaslo@hotmail.com>
2015-09-17 10:35:20 -0500

    Cambio por Vaslo Arreglo Menu Home ya llama al formulario en donde se ingresan los datos del participante

--- src/quienquieresermillonario/Home.java
+++ src/quienquieresermillonario/Home.java
@@ -1,13 +1,12 @@
 
 package quienquieresermillonario;
 
+import Formularios.IngresoDatosJugador;
+import app.UsersType.Participante;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author M O
- */
-public class Home extends javax.swing.JFrame {
+public class Home extends javax.swing.JFrame 
+{
 
     /**
      * Creates new form Home
@@ -23,10 +22,10 @@ public class Home extends javax.swing.JFrame {
 
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
+        Administrador = new javax.swing.JButton();
+        Participante = new javax.swing.JButton();
+        SalirHome = new javax.swing.JButton();
+        SeleccionaTuTipoDeUsuario = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
 
         jLabel3.setText("jLabel3");
@@ -47,65 +46,63 @@ public class Home extends javax.swing.JFrame {
         });
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                PresentadorActionPerformed(evt);
             }
         });
         getContentPane().add(jButton1);
         jButton1.setBounds(10, 130, 180, 40);
 
-        jButton2.setBackground(new java.awt.Color(51, 204, 255));
-        jButton2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jButton2.setText("Administrador");
-        jButton2.addMouseListener(new java.awt.event.MouseAdapter() {
+        Administrador.setBackground(new java.awt.Color(51, 204, 255));
+        Administrador.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        Administrador.setText("Administrador");
+        Administrador.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton2MouseClicked(evt);
+                AdministradorMouseClicked(evt);
             }
         });
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        Administrador.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                AdministradorActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2);
-        jButton2.setBounds(220, 130, 180, 40);
+        getContentPane().add(Administrador);
+        Administrador.setBounds(220, 130, 180, 40);
 
-        jButton3.setBackground(new java.awt.Color(51, 204, 255));
-        jButton3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jButton3.setText("Participante");
-        jButton3.setToolTipText("");
-        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+        Participante.setBackground(new java.awt.Color(51, 204, 255));
+        Participante.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        Participante.setText("Participante");
+        Participante.setToolTipText("");
+        Participante.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton3MouseClicked(evt);
+                ParticipanteMouseClicked(evt);
             }
         });
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        Participante.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                ParticipanteActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton3);
-        jButton3.setBounds(420, 130, 180, 40);
+        getContentPane().add(Participante);
+        Participante.setBounds(420, 130, 180, 40);
 
-        jButton4.setBackground(new java.awt.Color(51, 204, 255));
-        jButton4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        jButton4.setText("Salir");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
+        SalirHome.setBackground(new java.awt.Color(51, 204, 255));
+        SalirHome.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        SalirHome.setText("Salir");
+        SalirHome.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
+                SalirHomeActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton4);
-        jButton4.setBounds(220, 180, 180, 40);
+        getContentPane().add(SalirHome);
+        SalirHome.setBounds(220, 180, 180, 40);
 
-        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel2.setFont(new java.awt.Font("Shonar Bangla", 1, 32)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(51, 255, 51));
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("Selecciona tu tipo de usuario");
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(150, 50, 340, 50);
+        SeleccionaTuTipoDeUsuario.setBackground(new java.awt.Color(255, 255, 255));
+        SeleccionaTuTipoDeUsuario.setFont(new java.awt.Font("Tahoma", 1, 32)); // NOI18N
+        SeleccionaTuTipoDeUsuario.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        SeleccionaTuTipoDeUsuario.setText("Selecciona tu tipo de usuario:");
+        getContentPane().add(SeleccionaTuTipoDeUsuario);
+        SeleccionaTuTipoDeUsuario.setBounds(0, 30, 610, 50);
 
-        jLabel4.setIcon(new javax.swing.ImageIcon("C:\\Users\\M O\\Downloads\\descarga.jpg")); // NOI18N
         jLabel4.setText("jLabel4");
         getContentPane().add(jLabel4);
         jLabel4.setBounds(-300, 0, 990, 420);
@@ -113,49 +110,50 @@ public class Home extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-       // intentando que me lleve al otro formulario
-        //String Mensaje= " opcion elegida";
-        //if(jButton1.isSelected()){
-           // Mensaje=Mensaje+"Escogio presentador";
-        //}
-       Presentador P=new Presentador(); 
-       
-                new Presentador().setVisible(true);  
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void PresentadorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PresentadorActionPerformed
+       Presentador Pre=new Presentador(); 
+       Pre.setVisible(true);  
+    }//GEN-LAST:event_PresentadorActionPerformed
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-        // TODO add your handling code here:
+        
         JOptionPane.showMessageDialog(null, "Bienvenido Presentador");
+        
     }//GEN-LAST:event_jButton1MouseClicked
 
-    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
-        // TODO add your handling code here:
+    private void AdministradorMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_AdministradorMouseClicked
+        
         JOptionPane.showMessageDialog(null, "Bievenido Administrador");
-    }//GEN-LAST:event_jButton2MouseClicked
+        
+    }//GEN-LAST:event_AdministradorMouseClicked
 
-    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
-        // TODO add your handling code here:
-        //---------------------------------
-        JOptionPane.showMessageDialog(null, "Bievenido Participante");
-    }//GEN-LAST:event_jButton3MouseClicked
+    private void ParticipanteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ParticipanteMouseClicked
 
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
+        ///JOptionPane.showMessageDialog(null, "Bievenido Participante");
+        
+    }//GEN-LAST:event_ParticipanteMouseClicked
+
+    private void SalirHomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalirHomeActionPerformed
+        
         System.exit(0);
-    }//GEN-LAST:event_jButton4ActionPerformed
+    }//GEN-LAST:event_SalirHomeActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        Administrador A=new Administrador();
-        new Administrador().setVisible(true);
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void AdministradorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AdministradorActionPerformed
+        
+        Administrador Admin = new Administrador();
+        Admin.setVisible(true);
+    }//GEN-LAST:event_AdministradorActionPerformed
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-        Participante Pa=new Participante();
-        new Participante().setVisible(true);
-    }//GEN-LAST:event_jButton3ActionPerformed
+    private void ParticipanteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ParticipanteActionPerformed
+        
+        //Participante Pa = new Participante();
+        //Pa.setVisible(true);
+        
+        IngresoDatosJugador IDJ = new IngresoDatosJugador();
+        IDJ.setVisible(true);
+        this.setVisible(false);
+
+    }//GEN-LAST:event_ParticipanteActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -190,11 +188,11 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Administrador;
+    private javax.swing.JButton Participante;
+    private javax.swing.JButton SalirHome;
+    private javax.swing.JLabel SeleccionaTuTipoDeUsuario;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
